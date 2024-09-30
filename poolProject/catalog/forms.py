fd127ab3fc5eaa781254f8a81ed55368d520834b
yuri <110828443+LizunYuri@users.noreply.github.com>
2024-09-30 17:57:44 +0300

    сщььше30.09

--- poolProject/catalog/forms.py
+++ poolProject/catalog/forms.py
@@ -1,7 +1,7 @@
 import json
 from django import forms
 from django.contrib import admin
-from .models import ServicesModel
+from .models import ServicesModel, ShopCategoryModel, ShopElementModel
 
 class ServicesModelForm(forms.ModelForm):
     # Определяем поле как TextArea, чтобы ввести шаги вручную было удобнее
@@ -26,3 +26,23 @@ class ServicesModelForm(forms.ModelForm):
 @admin.register(ServicesModel)
 class ServicesAdmin(admin.ModelAdmin):
     form = ServicesModel
+
+
+
+
+# class TypeProductForm(forms.ModelForm):
+#     class Meta:
+#         model = ShopCategoryModel
+#         fields = ['type']
+#         widgets = {
+#             'type': forms.Select(attrs={'class': 'form-control'}),
+#         }
+
+# # Форма для ввода категории товара
+# class CategoryForm(forms.ModelForm):
+#     class Meta:
+#         model = ShopCategoryModel
+#         fields = ['category']
+#         widgets = {
+#             'category': forms.TextInput(attrs={'class': 'form-control'}),
+#         }
